d81167a2bf7a74633c25072946ff426d96be8dc2
Noel Lim <noellimx@gmail.com>
2022-01-30 18:50:32 +0800

    [TestDatabase]Connection should throw if initialize connection twice.

--- src/unit_testing/src/TestDatabase.cpp
+++ src/unit_testing/src/TestDatabase.cpp
@@ -3,23 +3,22 @@
 
 #include "catch.hpp"
 
-// Each cpp is a set of test cases for a specific component.
 namespace TestDatabase {
 void require(bool b, std::string &desc) {
   REQUIRE(b);
 }
-
-// The name of a test case should be unique and meaningful.
+TEST_CASE("[TestDatabase]Connection should throw if initialize connection twice."){
+  database::initialize();
+  CHECK_THROWS(database::initialize());
+}
 TEST_CASE("[TestDatabase]Table Procedure", "[Database.Procedure]") {
-  std::string testName = "[Test]DatabaseProcedure";
-  // Insert 3 procedure names. Expects exactly 3 procedure names stored in database.
   database::initialize();
 
   std::string procedureName1 = "procedure1";
   std::string procedureName2 = "procedure2";
 
-  auto * proc1 = new TokenProcedure(procedureName1);
-  auto * proc2 = new TokenProcedure(procedureName2);
+  auto *proc1 = new TokenProcedure(procedureName1);
+  auto *proc2 = new TokenProcedure(procedureName2);
 
   database::insertProcedure(proc1);
   database::insertProcedure(proc2);
@@ -27,6 +26,8 @@ TEST_CASE("[TestDatabase]Table Procedure", "[Database.Procedure]") {
   CHECK(database::isProcedureExist(proc2->getName()));
 
 }
+
+
 }
 
 
